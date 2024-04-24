dfde57241aafd24f7fc980284f8e1f81c425298b
dmytro babinenko <tw1nystudy@gmail.com>
2024-04-24 18:24:15 +0300

    12

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// const apple = "🍎";
+// const appleQuantity = 8;
+// const applePrice = 34;
+
+// const appleMessage = `${apple} total cost ${appleQuantity * applePrice}`;
+// console.log(appleMessage) ;
+
+// const cherry = "🍒";
+// const cherryQuantity = 3;
+// const cherryPrice = 72;
+// const cherryMessage = `${cherry} total cost ${cherryQuantity * cherryPrice}`;
+// console.log(cherryMessage);
+
+// function getTotalCost (productName, quantity, price){
+// return `${productName} total cost ${quantity * price}`;
+// }
+// console.log(getTotalCost(apple, appleQuantity, applePrice));
+// console.log(getTotalCost(cherry, cherryQuantity, cherryPrice));
+
+
+
+// function getTime(minutes) {
+// const hours = Math.floor(minutes / 60);
+// const remainingMinutes = minutes % 60;
+// return `${hours.toString().padStart(2, "0")} : ${remainingMinutes.toString().padStart(2, "0")}`
+// } 
+
+// console.log(getTime (70));
+// console.log(getTime (450));
+// console.log(getTime (1441));
+
