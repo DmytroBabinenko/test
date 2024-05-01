41dc3bda47308be25e3006a0c4bd93f02f02b16e
dmytro babinenko <tw1nystudy@gmail.com>
2024-05-01 06:57:49 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,82 +1,26 @@
-// const apple = "🍎";
-// const appleQuantity = 8;
-// const applePrice = 34;
-
-// const appleMessage = `${apple} total cost ${appleQuantity * applePrice}`;
-// console.log(appleMessage) ;
-
-// const cherry = "🍒";
-// const cherryQuantity = 3;
-// const cherryPrice = 72;
-// const cherryMessage = `${cherry} total cost ${cherryQuantity * cherryPrice}`;
-// console.log(cherryMessage);
-
-// function getTotalCost (productName, quantity, price){
-// return `${productName} total cost ${quantity * price}`;
+// let myName
+// console.log (myName)
+// myName = 'Dima'
+// console.log (myName)
+// console.log (console.log(10))
+// const myName = 'dima'
+// myName = ''
+// console.log(myName)
+// const objectA = {
+//     a: 10,
+//     b: true
 // }
-// console.log(getTotalCost(apple, appleQuantity, applePrice));
-// console.log(getTotalCost(cherry, cherryQuantity, cherryPrice));
-
-
-
-// function getTime(minutes) {
-// const hours = Math.floor(minutes / 60);
-// const remainingMinutes = minutes % 60;
-// return `${hours.toString().padStart(2, "0")} : ${remainingMinutes.toString().padStart(2, "0")}`
-// } 
-
-// console.log(getTime (70));
-// console.log(getTime (450));
-// console.log(getTime (1441));
-
-// let value;
-// console.log('1', value)
-// value = 1;
-// console.log('2', value)
-// value = 'hello';
-// console.log('3', value)
-
-
-// function foo(){
-    
-    
-//     function boo(){
-//         let a = 10;
-//         console.log('boo', a)
-//         return a
+// const copyOfA = objectA
+// copyOfA.c = 'abc'
+// const a = ( ) => {
+//     console.log('Hey there')}
+//     a() 
+//     a = 10
+//     a() 
+// const myCity = {
+//     city: 'New York',
+//     cityGreeting: function () {
+//         console.log('Greetings!!')
 //     }
-//     boo()
-//     console.log('foo', a)
 // }
-// foo()
-// console.log('global', a)
-
-// (() => {
-//     console.log('hello')
-// })();
-
-// function foo(){
-//     var a = 10;
-// }
-// foo()
-// console.log(a)
-
-// const value = 1;
-// const value2 = 2.5;
-// const value3 = NaN;
-
-// //string
-
-// const str = `${value}`;
-// const str1 = 'some text "Say" ';
-// const str2 = "${value}";
-
-
-// const value = '26.45a45';
-// console.log(Number.parseFloat(value))
-
-
-
-
-
-
+// myCity.cityGreeting()
