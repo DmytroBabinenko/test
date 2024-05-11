7900056d43732ca8c121d83576416626b3e31cc7
dmytro babinenko <tw1nystudy@gmail.com>
2024-05-11 09:00:15 +0300

    123

--- js/index.js
+++ js/index.js
@@ -1,26 +1,89 @@
-// let myName
-// console.log (myName)
-// myName = 'Dima'
-// console.log (myName)
-// console.log (console.log(10))
-// const myName = 'dima'
-// myName = ''
-// console.log(myName)
-// const objectA = {
-//     a: 10,
-//     b: true
+// const personOne = {
+//     name: 'Bob',
+//     age: 21
 // }
-// const copyOfA = objectA
-// copyOfA.c = 'abc'
-// const a = ( ) => {
-//     console.log('Hey there')}
-//     a() 
-//     a = 10
-//     a() 
-// const myCity = {
-//     city: 'New York',
-//     cityGreeting: function () {
-//         console.log('Greetings!!')
-//     }
+
+// function increasaePersonAge(person) {
+//     const updatedPerson = Object.assign ({}, person)
+//     updatedPerson.age += 1
+//     return updatedPerson
 // }
-// myCity.cityGreeting()
+
+// const updatedPersonOne = increasaePersonAge(personOne)
+// console.log(personOne.age)
+// console.log(updatedPersonOne.age)
+
+
+//
+// console.log(Boolean(1))
+// console.log(Boolean(!!1))
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
+// console.log(!true)
+// console.log(!false)
+// console.log(Number("18"))
+// console.log(1 + 1)
+// console.log(+ '1')
+// console.log(Number(+"1" === 1));
+// console.log(Number('1') === 1)
+
+// const str = "25.7a32px";
+// console.log(typeof str)
+// console.log(Number.isNaN(Number(str)))
+// console.log(isNaN(str))
+
+// const value = 11.99;
+// console.log(Math.ceil(value))
+// console.log(Math.floor(value))
+// console.log(Math.round(value))
+
+// console.log(Math.pow(5, 4))
+// console.log(5 ** 4)
+// console.log(add(10, 20))
+// function add(val1, val2) {
+// return val1 + val2;
+// }
+// const apple = "apple";
+// const appleQuantity = 3;
+// const applePrice = 72;
+
+
+// const cherry = "cherry";
+// const cherryQuantity = 3;
+// const cherryPrice = 72;
+// // const cherryMessage = `${cherry} total cost ${cherryQuantity * cherryPrice}`;
+// // console.log(cherryMessage);
+// function getTotalCost (productName, quantity, price) {
+// return `${productName} total cost ${quantity * price}`
+// }
+// console.log(getTotalCost(cherry, cherryQuantity, cherryPrice));
+// console.log(getTotalCost(apple, appleQuantity, applePrice));
+
+
+// function getTime(minutes) {
+// const hours = Math.floor (minutes / 60);
+// const remainingMinutes = minutes % 60;
+// return `${hours.toString().padStart(2, '0')} : ${remainingMinutes.toString().padStart(2, '0')}`;
+// console.log(typeof remainingMinutes);
+// }
+// console.log(getTime(70))
+// console.log(getTime(450))
+// console.log(getTime(1441))
+
+
+// function anotherFunction() {
+
+// }
+// function fnWithCallback(callbackFunction){
+//     callbackFunction()
+// }
+
+// fnWithCallback(anotherFunction)
+
+
+
+// console.log(false && 1);
+// console.log(NaN && 2);
+// console.log(1 && 5 && 23);
+
+
